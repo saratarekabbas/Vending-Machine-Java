ee521ff3c532422cecede6e8f74c89de3efcaf93
saratarekabbas <saraatarekk@icloud.com>
2022-06-26 14:40:53 +0800

    Removed unused imports

--- src/main/java/VMPackage/CustomerControlPanelJFrame.java
+++ src/main/java/VMPackage/CustomerControlPanelJFrame.java
@@ -2,14 +2,12 @@ package VMPackage;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /*
