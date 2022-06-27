fe5e086ac4ef8638daa7da826e640e24a50185fb
saratarekabbas <saraatarekk@icloud.com>
2022-06-27 13:04:46 +0800

    All is working- b4 changing UpdateStock1

--- src/main/java/VMPackage/MachinerySimulationControlPanelJFrame.java
+++ src/main/java/VMPackage/MachinerySimulationControlPanelJFrame.java
@@ -9,7 +9,6 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
--- src/main/java/VMPackage/SimulatorControlPanelJFrame.java
+++ src/main/java/VMPackage/SimulatorControlPanelJFrame.java
@@ -1,8 +1,4 @@
 package VMPackage;
-
-import java.io.File;
-import java.io.IOException;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
